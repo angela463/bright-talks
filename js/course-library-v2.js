600db328c107c0650063556369ea0c40397e24eb
Angela Ariss <angela.ariss@covenanteyes.com>
2026-05-06 16:02:10 -0400

    Add "Open player" button to course card actions and update styles in course-player-v2 and course-experience.css for improved visual consistency and user experience.

--- js/course-library-v2.js
+++ js/course-library-v2.js
@@ -72,6 +72,7 @@
       '    </div>' +
       '    <div class="course-v2-card__actions">' +
       '      <a class="btn btn-primary" href="course-detail-v2.html?course=' + encodeURIComponent(course.id) + '">' + ctaLabel + '</a>' +
+      '      <a class="btn btn-secondary" href="course-player-v2.html?course=' + encodeURIComponent(course.id) + '&module=0&lesson=0">Open player</a>' +
       '    </div>' +
       '  </div>' +
       '</article>';
